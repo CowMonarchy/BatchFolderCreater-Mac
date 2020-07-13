0934ea8df8c9f773d714715b4040ad0286ce91bc
FLin <>
2020-07-12 23:24:56 -0500

    Adjusted UI, User no longer need dir entry

--- FolderStruct.py
+++ FolderStruct.py
@@ -12,21 +12,9 @@ folderNum = 'None'
 foldersName = "             Please enter a name             "
 folderPath = sys.argv[1]
 
-#testPath = sys.argv
 
 
 #--Functions--
-def SelectDirectory():
-    dire = os.getcwd()
-
-    dirField.delete(0, 'end')
-    dirField.config(fg='white')
-    dirField.insert(0, dire)
-    
-    return dire
-
-
-
 def EnterName(*kwargs):
     nameField.delete(0, 'end')
     nameField.config(fg='white')
@@ -47,16 +35,12 @@ def ConditionCheck(*kwargs):
     global quanSpn
     
     folderNum = windowVar.get()
-    folderPath = dirField.get()
     foldersName = nameField.get()
 
     if folderNum.isdigit() == False:
         submitBtn.config(state='disabled')
         return
     if  len(foldersName) == 0 or foldersName.isspace() or foldersName == "             Please enter a name             " :
-        submitBtn.config(state='disabled')
-        return
-    if len(folderPath) == 0 or folderPath.isspace() or folderPath == "                        Select a folder                        " :
         submitBtn.config(state='disabled')
         return
     
@@ -73,7 +57,6 @@ def CreateFolders():
     global quanSpn
     
     folderNum = windowVar.get()
-    folderPath = dirField.get()
     foldersName = nameField.get()
 
 
@@ -100,24 +83,9 @@ window.title('Folder Creator')
 window.attributes('-alpha', 0.97)
 window.attributes('-transparent', 'true')
 window.config(background="#2B2929")
-window.maxsize(width=410, height=80)
-window.minsize(width=410, height=80)
+window.maxsize(width=410, height=35)
+window.minsize(width=410, height=35)
 #Main Window__________________________________________
-
-
-
-#Choosing Directory 
-dirField = tk.Entry(window, width=36)
-dirField.config(highlightthickness=0, font=('San Fransisco', 12), bd=0, bg='#747272', fg='#908E8E')
-dirField.insert(0, folderPath)
-dirField.bind("<Leave>", ConditionCheck)
-dirField.place(x=109, y=13)
-
-dirBtn = tk.Button(window, command = lambda : (folderPath := SelectDirectory()), text='Choose')
-dirBtn.config(highlightthickness=0, font=('San Fransisco', 12), width=10, bg='#727171', fg='black')
-dirBtn.bind("<Leave>", ConditionCheck)
-dirBtn.place(x=15, y=13)
-#Choosing Directory__________________________________________________________________
 
 
 
@@ -127,7 +95,7 @@ nameField.config(highlightthickness=0, font=('San Fransisco', 12), bd=0, bg='#74
 nameField.insert(0, foldersName)
 nameField.bind("<Button-1>", EnterName)
 nameField.bind("<Leave>", ConditionCheck)
-nameField.place(x=109, y=48)
+nameField.place(x=109, y=9)
 #Entering Name_______________________________________________________________________________________________
 
 
@@ -139,7 +107,7 @@ quanSpn = tk.Spinbox(window, textvariable=windowVar, highlightthickness=0)
 quanSpn.config(increment=1, width=3, from_=2, to=100, bg='#747272', fg='#908E8E', bd=0)
 quanSpn.bind("<Button-1>", ChooseQuan)
 quanSpn.bind("<Leave>", ConditionCheck)
-quanSpn.place(x=348, y=47)
+quanSpn.place(x=348, y=8)
 #Selecting Quanity_____________________________________________________________________________________________________________
 
 
@@ -147,7 +115,7 @@ quanSpn.place(x=348, y=47)
 #Creating Folders 
 submitBtn = tk.Button(window, text='Create', command = lambda : windowVar.set(CreateFolders()))
 submitBtn.config(highlightthickness=0, state='disabled', font=('San Fransisco', 12), width=10, bd=0)
-submitBtn.place(x=15, y=48)
+submitBtn.place(x=15, y=8)
 #Creating Folders____________________________________________________________________________________________________________________________
 
 
